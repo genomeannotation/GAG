d6aae4452a60e500eefd6ad615501a6611f417fe
Theodore DeRego <tderego94@gmail.com>
2013-08-08 10:01:21 -1000

    Some sqlite3 stuff

--- gad_generator.py
+++ gad_generator.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python
 
-print("Hello, world!")
+import sqlite3
+
+print('Hello, world!')
+
+# Connect to the database.
+db = sqlite3.connect('example.db')
+
+# Grab the database's cursor to execute commands
+c = db.cursor()
+
+# Create the table
+c.execute('CREATE TABLE Cars(Id integer primary key, Name text, Cost integer)')
+
+# Commit the changes to the database
+db.commit()
+
+# Close the database when we're done.
+db.close()
