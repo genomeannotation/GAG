fa0a546737d9b41f4c49e1a966b4ffd094c4cf4c
Brian Hall <bhall7@hawaii.edu>
2013-08-08 15:22:23 -1000

    added fasta_reader

---
+++ fasta_reader.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sqlite3
+import Bio
+from Bio import SeqIO
+
+class FastaReader:
+
+
+	def read_sequences_into_db(self, filename, db_name):
+		conn = sqlite3.connect(db_name)
+		c = conn.cursor()
+		c.execute('''CREATE TABLE IF NOT EXISTS Sequences
+					(SequenceID text, Sequence text)''')
+
+		for seq_record in SeqIO.parse(filename, "fasta"):
+			id_and_sequence = [str(seq_record.id), str(seq_record.seq)]
+			c.execute('''INSERT INTO Sequences VALUES (?, ?)''', id_and_sequence)
+			conn.commit()
