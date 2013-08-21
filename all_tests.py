7a287a32df43ecbf1a8736cc033da6e7599b6b0d
Theodore DeRego <tderego94@gmail.com>
2013-08-21 10:49:06 -1000

    Fixed trinotate reader, added unit test.

--- all_tests.py
+++ all_tests.py
@@ -3,6 +3,7 @@
 import unittest
 from gff_reader import GffReader
 from fasta_reader import FastaReader
+from trinotate_reader import TrinotateReader
 from feature_tbl_writer import FeatureTblWriter
 from sqlite_wrapper import SqliteWrapper
 import sqlite3
@@ -50,6 +51,24 @@ class TestStuff(unittest.TestCase):
         self.assertEqual("scaffold00081", second_sequence_id)
         second_sequence = second_row[1]
         self.assertEqual("gcagtttttCGCcGAAAACCCAGAAAAATGGCAAG", second_sequence[0:35])
+
+    def test_trinotate_reader(self):
+        con = sqlite3.connect(':memory:')
+        test_reader = TrinotateReader()
+        test_reader.read_into_db("test_files/test.trinotate.xls", con)
+        cur = con.cursor()
+        cur.execute("select * from trinotate")
+        first_row = cur.fetchone()
+        self.assertEqual('BDOR_007864-RA', first_row[0])
+        self.assertEqual('comp10010_c0', first_row[1])
+        self.assertEqual('comp10010_c0_seq1:3-1250(-)', first_row[2])
+        self.assertEqual('sp|Q7Z4T9|AAT1_HUMAN`Q7Z4T9`Q:533-644,H:400-513`37.39%ID`E:9e-08`RecName: Full=Protein MAATS1; AltName: Full=AMY-1-associating protein expressed in testis 1; Short=AAT-1; AltName: Full=MYCBP/AMY-1-associated testis-expressed protein 1;`Eukaryota; Metazoa; Chordata; Craniata; Vertebrata; Euteleostomi; Mammalia; Eutheria; Euarchontoglires; Primates; Haplorrhini; Catarrhini; Hominidae; Homo.', first_row[3])
+        self.assertEqual('.', first_row[4])
+        self.assertEqual('.', first_row[5])
+        self.assertEqual('.', first_row[6])
+        self.assertEqual('NOG84773^ Amy-1-Associating protein expressed in testis 1 (AAT-1)', first_row[7])
+        self.assertEqual('GO:0005739^cellular_component^mitochondrion', first_row[8])
+        self.assertEqual('MAQPSHHKLAKRVFKSVYDAECSTADEKSDLKMFKNARNKATTAK', first_row[9][:45])
 
     def test_tbl_writer(self):
         con = sqlite3.connect(':memory:')
--- trinotate_reader.py
+++ trinotate_reader.py
@@ -7,13 +7,15 @@ class TrinotateReader:
 
     ## Loads the gff file into a sqlite database
     def read_into_db(self, file_name, db_conn):
-        # Create the sqlite database: component | trans_derived | prot_id | top_blast_hit, pfam, tmhmm, eggnog, gene_ontology, prot_seq
+        # Create the sqlite database: prot_id | component | trans_derived | top_blast_hit | pfam | signalp | tmhmm | eggnog | gene_ontology | prot_seq
         db_cur = db_conn.cursor()
-        db_cur.execute('CREATE TABLE trinotate(prot_id TEXT PRIMARY KEY, component TEXT, trans_derived TEXT, top_blast_hit TEXT, pfam TEXT, tmhmm TEXT, eggnog TEXT, gene_ontology TEXT, prot_seq TEXT)')
+        db_cur.execute('CREATE TABLE trinotate(prot_id TEXT PRIMARY KEY, component TEXT, trans_derived TEXT, top_blast_hit TEXT, pfam TEXT, signalp TEXT, tmhmm TEXT, eggnog TEXT, gene_ontology TEXT, prot_seq TEXT)')
 
         with open(file_name, 'r') as gff:
             reader = csv.reader(gff, delimiter='\t')
             for line in reader:
+                if len(line) == 0 or line[0][0] == '#': # Skip commented lines
+                    continue
                 db_cur.execute('INSERT INTO trinotate VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [line[2], line[0], line[1], line[3], line[4], line[5], line[6], line[7], line[8], line[9]])      
 
         return db_conn
