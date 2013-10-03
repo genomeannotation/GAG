b3ee42bbb2eca6090892fdbc7297a3187cbc0961
Brian <brian@reallymany.com>
2013-10-03 13:37:56 -1000

    plus 2 fns, plus 4 vars

--- feature_tbl_writer.py
+++ feature_tbl_writer.py
@@ -71,6 +71,11 @@ class FeatureTblWriter:
     def generate_header_line(self):
         return '>Feature SeqId\n'
 
+    def write_sequence_header(self, openfile, seq_id, seq):
+        openfile.write('>Feature '+seq_id+'\n')
+        openfile.write('1\t'+str(len(seq))+'\tREFERENCE\n')
+        openfile.write('\t\t\tPBARC\t12345\n')
+
     def write_to_file(self, db_conn, fileName, blacklist):
         db_conn.create_function('regexp', 2, regexp)
 
@@ -83,20 +88,17 @@ class FeatureTblWriter:
             # Get the sequence ids
             db_cur.execute('SELECT * FROM fasta')
 
-            i = 0
-            for seq in db_cur.fetchall():
+            for i, seq in enumerate(db_cur.fetchall()):
                 sequence_id = seq[0]
                 sequence = seq[1]
                 i += 1
                 if i%100 == 0:
                     print(i)
 
-                f.write('>Feature '+sequence_id+'\n')
-                f.write('1\t'+str(len(sequence))+'\tREFERENCE\n')
-                f.write('\t\t\tPBARC\t12345\n')
-
-                # Create the temporary table
-                # db_cur.execute('CREATE TABLE tmp_cur_seq(id TEXT PRIMARY KEY, seq_id TEXT, type TEXT, start INT, stop INT, name TEXT, parent TEXT)')
+                #f.write('>Feature '+sequence_id+'\n')
+                #f.write('1\t'+str(len(sequence))+'\tREFERENCE\n')
+                #f.write('\t\t\tPBARC\t12345\n')
+                self.write_sequence_header(f, sequence_id, sequence)
                 
                 # TODO: Match columns up with gff columns (i.e. strand isn't last in gff table)
                 magic_query = "CREATE TEMP TABLE tmp_cur_seq AS SELECT id, seq_id, type, start, stop, name, parent, strand, phase FROM gff WHERE seq_id=?"
@@ -109,7 +111,8 @@ class FeatureTblWriter:
                 genes = db_cur.fetchall()
 
                 for gene in genes:
-                    if gene[5] in blacklist:
+                    gene_name = gene[5]
+                    if gene_name in blacklist:
                         continue
 
                     # Whether or not the gene is good to write
@@ -118,7 +121,7 @@ class FeatureTblWriter:
                     cds_entries = []
 
                     # Find a trinotate entry to give us the gene id for this gene
-                    db_cur.execute('SELECT top_blast_hit FROM trinotate WHERE prot_id REGEXP ? LIMIT 1', [gene[5]])
+                    db_cur.execute('SELECT top_blast_hit FROM trinotate WHERE prot_id REGEXP ? LIMIT 1', [gene_name])
                     trinotate = db_cur.fetchone()
 
                     # Write the gene
@@ -129,7 +132,7 @@ class FeatureTblWriter:
                     if gene[8] != '.':
                         gene_entry.set_phase(int(gene[8]))
                     gene_entry.add_coordinates(gene[3], gene[4])
-                    gene_ann = 'locus_tag='+gene[5]
+                    gene_ann = 'locus_tag='+gene_name
                     if trinotate:
                         gene_ann += ','+parse_blast_hit_gene(trinotate[0])
                     for annot in gene_ann.split(','):
