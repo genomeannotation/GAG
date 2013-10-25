c77790b3451fe23153b118f1ad472f136859de3e
Brian Hall <bhall7@hawaii.edu>
2013-10-24 14:59:49 -1000

    exon too

--- class_tests.py
+++ class_tests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import unittest
-from feature_classes import CDS
+from feature_classes import CDS, Exon
 
 class TestFeatureClasses(unittest.TestCase):
 
@@ -35,8 +35,43 @@ class TestFeatureClasses(unittest.TestCase):
         expected4 = "sctg_0080_0020\tmaker\tCDS\t5249\t6565\t.\t+\t0\tID=11;Name=BDOR_007864-RA:cds:3;Parent=2\n"
         expected5 = "sctg_0080_0020\tmaker\tCDS\t6630\t7436\t.\t+\t0\tID=12;Name=BDOR_007864-RA:cds:4;Parent=2\n"
         expected = expected1 + expected2 + expected3 + expected4 + expected5
-        result = test_cds1.to_gff(seq_name="sctg_0080_0020", source="maker", strand='+')
-        self.assertEqual(expected, result)
+        actual = test_cds1.to_gff(seq_name="sctg_0080_0020", source="maker", strand='+')
+        self.assertEqual(expected, actual)
+
+        # test to_tbl
+        # TODO when add annotations stuff
+
+    def test_Exon(self):
+        # test constructor
+        test_ids1 = [3, 4, 5, 6, 7]
+        test_names1 = ["BDOR_007864-RA:exon:0", "BDOR_007864-RA:exon:1", "BDOR_007864-RA:exon:2", "BDOR_007864-RA:exon:3", "BDOR_007864-RA:exon:4"]
+        test_indices1 = [[3734, 4034], [4092, 4332], [4399, 5185], [5249, 6565], [6630, 7436]]
+        test_scores1 = [0.9, 0.9, 0.9, 0.9, 0.9]
+        test_parent_id1 = 2
+        test_exon1 = Exon(ids=test_ids1, names=test_names1, indices=test_indices1, scores=test_scores1, parent_id=test_parent_id1)
+        self.assertEqual('Exon', test_exon1.__class__.__name__)
+        
+        # test .length
+        self.assertEqual(3453, test_exon1.length())
+
+        # test .adjust_indices
+        test_exon1.adjust_indices(7)
+        self.assertEqual(4339, test_exon1.indices[1][1])
+        # adjust them back...
+        test_exon1.adjust_indices(-7)
+        self.assertEqual(4332, test_exon1.indices[1][1])
+
+        # test .to_gff
+        expected1 = "sctg_0080_0020\tmaker\texon\t3734\t4034\t0.9\t+\t.\tID=3;Name=BDOR_007864-RA:exon:0;Parent=2\n"
+        expected2 = "sctg_0080_0020\tmaker\texon\t4092\t4332\t0.9\t+\t.\tID=4;Name=BDOR_007864-RA:exon:1;Parent=2\n"
+        expected3 = "sctg_0080_0020\tmaker\texon\t4399\t5185\t0.9\t+\t.\tID=5;Name=BDOR_007864-RA:exon:2;Parent=2\n"
+        expected4 = "sctg_0080_0020\tmaker\texon\t5249\t6565\t0.9\t+\t.\tID=6;Name=BDOR_007864-RA:exon:3;Parent=2\n"
+        expected5 = "sctg_0080_0020\tmaker\texon\t6630\t7436\t0.9\t+\t.\tID=7;Name=BDOR_007864-RA:exon:4;Parent=2\n"
+        expected = expected1 + expected2 + expected3 + expected4 + expected5
+        actual = test_exon1.to_gff(seq_name="sctg_0080_0020", source="maker", strand='+')
+        self.assertEqual(expected, actual)
+        
+
 
 ##########################
 if __name__ == '__main__':
--- feature_classes.py
+++ feature_classes.py
@@ -17,6 +17,7 @@ class CDS:
         self.indices = indices
         self.frames = frames
         self.parent_id = parent_id
+        self.annotations = []
 
     def length_of_shortest_segment(self):
         min_length = length_of_segment(self.indices[0])
@@ -46,3 +47,35 @@ class CDS:
             result += "ID=" + str(self.ids[i]) + ";Name=" + self.names[i]
             result += ";Parent=" + str(self.parent_id) + "\n"
         return result
+
+
+class Exon:
+
+    def __init__(self, ids, names, indices, scores, parent_id):
+        self.ids = ids
+        self.names = names
+        self.indices = indices
+        self.scores = scores
+        self.parent_id = parent_id
+
+    def length(self):
+        length = 0
+        for index_pair in self.indices:
+            length += length_of_segment(index_pair)
+        return length
+
+    def adjust_indices(self, n):
+        self.indices = [adjust_index_pair(pair, n) for pair in self.indices]
+
+    def to_gff(self, seq_name, source, strand):
+        result = ""
+        for i in range(len(self.indices)):
+            result += seq_name + "\t" + source + "\t" + "exon" + "\t"
+            result += str(self.indices[i][0]) + "\t" + str(self.indices[i][1]) + "\t"
+            result += str(self.scores[i]) + "\t" + strand + "\t" + "." + "\t"
+            result += "ID=" + str(self.ids[i]) + ";Name=" + self.names[i]
+            result += ";Parent=" + str(self.parent_id) + "\n"
+        return result
+
+
+
