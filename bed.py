baf70ed3a36f40641db6c7baae52bcf75e9cb549
Brian Hall <bhall7@hawaii.edu>
2013-10-25 10:30:57 -1000

    more bed methods

--- bed.py
+++ bed.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# Bed contains a dict called 'entries' whose keys are chromosomes/contigs
+# and whose values are lists of coordinates
+# Example: awesome_entry = {'sctg_0002_0347': [1, 4307]}
 class Bed:
 
 
@@ -11,3 +14,6 @@ class Bed:
 
     def add_entry(self, key, val):
         self.entries[key] = val
+
+    def contains(self, chrom):
+        return chrom in self.entries
--- bed_tests.py
+++ bed_tests.py
@@ -12,11 +12,15 @@ class TestBed(unittest.TestCase):
         self.assertEqual('Bed', test_bed1.__class__.__name__)
         self.assertFalse(test_bed1.entries)
 
-        # test add_entry
+        # test .add_entry
         test_key1 = "sctg_0002_0347"
         test_vals1 = [1, 4307]
         test_bed1.add_entry(test_key1, test_vals1)
         self.assertEqual(1, test_bed1.entries['sctg_0002_0347'][0])
+
+        # test .contains
+        self.assertTrue(test_bed1.contains('sctg_0002_0347'))
+        self.assertFalse(test_bed1.contains('totally made-up key'))
 
 
 
