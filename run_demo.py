2f37f7cfd376b7c69d473af779899728dabc295e
Brian Hall <bhall7@hawaii.edu>
2013-11-07 13:56:17 -1000

    changed demo.gff to make it more challenging

--- run_demo.py
+++ run_demo.py
@@ -24,24 +24,8 @@ with open(bed_filename, 'rb') as bedfile:
     bedreader = csv.reader(bedfile, delimiter='\t')
     bed.read_file(bedreader)
 
-
-print("here is the gff I read...")
-for gene in gff.genes:
-    print(gene.to_gff())
-
-print("\n")
-print("here is the bed...")
-print(str(bed))
-
-print("okay, last gene's first mrna:")
-print(str(gff.genes[len(gff.genes)-1].mrnas[0]))
-
 gff.apply_bed(bed)
 gff.remove_empty_genes()
 
-print("okay, last gene's first mrna:")
-print(str(gff.genes[len(gff.genes)-1].mrnas[0]))
-print("\n")
-print("and here is the gff after applying changes")
 for gene in gff.genes:
-    print(gene.to_gff())
+    sys.stdout.write(gene.to_gff())
