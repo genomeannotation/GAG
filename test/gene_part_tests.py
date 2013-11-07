225306bc9bd321ccacda566417b454532b181ba3
Brian Hall <bhall7@hawaii.edu>
2013-11-06 17:00:29 -1000

    refactored test_gene_part and test_cds, still need to do test_exon

--- test/gene_part_tests.py
+++ test/gene_part_tests.py
@@ -6,69 +6,75 @@ from src.feature_classes import GenePart, CDS, Exon, MRNA, Gene
 
 class TestGenePart(unittest.TestCase):
 
-    def test_GenePart(self):
-        # test constructor
-        gp1 = GenePart()
-        self.assertEqual(0, len(gp1.indices))
-        self.assertFalse(gp1.score)
-        self.assertFalse(gp1.parent_id)
-        gp2 = GenePart(feature_type='CDS', indices=[1, 44])
-        self.assertEqual(1, len(gp2.indices))
-        self.assertEqual('CDS', gp2.feature_type)
+    def setUp(self):
+        self.gp1 = GenePart()
+        self.gp2 = GenePart(feature_type='CDS', indices=[1, 44])
+        self.gp2.add_indices([65, 103])
+        self.gp2.name = ['cds:0', 'cds:1']
+        self.gp2.identifier = ['foo1', 'foo2']
+        self.gp2.parent_id = 'mama'
+        self.gp3 = GenePart(feature_type='exon')
 
-        # test .add_indices
-        gp2.add_indices([65, 103])
-        self.assertEqual(2, len(gp2.indices))
-        gp3 = GenePart(feature_type='exon')
-        self.assertEqual(0, len(gp3.indices))
-        gp3.add_indices([77, 144])
-        self.assertEqual(1, len(gp3.indices))
+    def test_constructor(self):
+        self.gp1 = GenePart()
+        self.assertEquals(0, len(self.gp1.indices))
+        self.assertFalse(self.gp1.score)
+        self.assertFalse(self.gp1.parent_id)
+        self.assertEquals('CDS', self.gp2.feature_type)
+
+    def test_add_indices(self):
+        self.assertEquals(2, len(self.gp2.indices))
+        self.gp2.add_indices([150, 197])
+        self.assertEquals(3, len(self.gp2.indices))
+
+        self.assertEquals(0, len(self.gp3.indices))
+        self.gp3.add_indices([77, 144])
+        self.assertEquals(1, len(self.gp3.indices))
         # error check
-        self.assertRaises(ValueError, gp3.add_indices, 7)
-        self.assertRaises(ValueError, gp3.add_indices, 'foo')
+        self.assertRaises(ValueError, self.gp3.add_indices, 7)
+        self.assertRaises(ValueError, self.gp3.add_indices, 'foo')
 
-        # test .add_name
-        self.assertEqual(0, len(gp3.name))
-        gp3.add_name('BDOR_007864-RA:cds:0')
-        self.assertEqual(1, len(gp3.name))
+    def test_add_name(self):
+        self.assertEquals(0, len(self.gp3.name))
+        self.gp3.add_name('BDOR_007864-RA:cds:0')
+        self.assertEquals(1, len(self.gp3.name))
 
-        # test .add_identifier
-        self.assertEqual(0, len(gp3.identifier))
-        gp3.add_identifier('7')
-        self.assertEqual(1, len(gp3.identifier))
+    def test_add_identifier(self):
+        self.assertEquals(0, len(self.gp3.identifier))
+        self.gp3.add_identifier('7')
+        self.assertEquals(1, len(self.gp3.identifier))
 
-        # test .length
-        self.assertEqual(83, gp2.length())
+    def test_length(self):
+        self.assertEquals(83, self.gp2.length())
         # what if no indices at all?
-        self.assertFalse(gp1.length())
+        self.assertFalse(self.gp1.length())
 
-        # test .adjust_indices
-        gp2.adjust_indices(10)
-        self.assertEqual(54, gp2.indices[0][1])
+    def test_adjust_indices(self):
+        self.gp2.adjust_indices(10)
+        self.assertEquals(54, self.gp2.indices[0][1])
         # now put it back
-        gp2.adjust_indices(-10)
-        self.assertEqual(65, gp2.indices[1][0])
+        self.gp2.adjust_indices(-10)
+        self.assertEquals(65, self.gp2.indices[1][0])
 
-        # test .length_of_shortest_segment
-        self.assertEqual(39, gp2.length_of_shortest_segment())
+    def test_length_of_shortest_segment(self):
+        self.assertEquals(39, self.gp2.length_of_shortest_segment())
         # what if no indices?
-        self.assertFalse(gp1.length_of_shortest_segment())
+        self.assertFalse(self.gp1.length_of_shortest_segment())
 
+    def test_generate_attribute_entry(self):
         # test .generate_attribute_entry
-        gp2.identifier = ['foo1', 'foo2']
-        gp2.parent_id = 'mama'
-        expected = "ID=foo2;Parent=mama\n"
-        self.assertEqual(expected, gp2.generate_attribute_entry(1))
-        gp2.name = ['cds:0', 'cds:1']
         expected = "ID=foo1;Name=cds:0;Parent=mama\n"
-        self.assertEqual(expected, gp2.generate_attribute_entry(0))
+        self.assertEquals(expected, self.gp2.generate_attribute_entry(0))
+        expected = "ID=foo2;Name=cds:1;Parent=mama\n"
+        self.assertEquals(expected, self.gp2.generate_attribute_entry(1))
         # what if index out of range?
-        self.assertFalse(gp2.generate_attribute_entry(2))
+        self.assertFalse(self.gp2.generate_attribute_entry(2))
         # what if no identifier or parent_id?
-        self.assertFalse(gp1.generate_attribute_entry(0))
-        gp1.parent_id = 'dad'
-        self.assertFalse(gp1.generate_attribute_entry(0))
+        self.assertFalse(self.gp1.generate_attribute_entry(0))
+        self.gp1.parent_id = 'dad'
+        self.assertFalse(self.gp1.generate_attribute_entry(0))
 
+    def test_to_gff(self):
         # test .to_gff
         seq_name = "sctg_0001_0001"
         source = 'maker'
@@ -77,79 +83,94 @@ class TestGenePart(unittest.TestCase):
         expected += "ID=foo1;Name=cds:0;Parent=mama\n"
         expected += "sctg_0001_0001\tmaker\tCDS\t65\t103\t.\t+\t.\t"
         expected += "ID=foo2;Name=cds:1;Parent=mama\n"
-        actual = gp2.to_gff(seq_name=seq_name, source=source, strand=strand)
-        self.assertEqual(expected, actual)
+        actual = self.gp2.to_gff(seq_name=seq_name, source=source, strand=strand)
+        self.assertEquals(expected, actual)
         # what if no indices, etc.?
-        self.assertFalse(gp1.to_gff(seq_name="foo", source="bar", strand=":)"))
+        self.assertFalse(self.gp1.to_gff(seq_name="foo", source="bar", strand=":)"))
         
+class TestCDS(unittest.TestCase):
 
-    def test_CDS(self):
-        # test constructor
-        test_indices1 = [3734, 4034]
-        extra_indices = [[4092, 4332], [4399, 5185], [5249, 6565], [6630, 7436]]
-        test_name1 = "BDOR_007864-RA:cds:0"
-        extra_names = ["BDOR_007864-RA:cds:1", "BDOR_007864-RA:cds:2", "BDOR_007864-RA:cds:3", "BDOR_007864-RA:cds:4"]
+    def setUp(self):
+        self.test_indices1 = [3734, 4034]
+        self.extra_indices = [[4092, 4332], [4399, 5185], [5249, 6565], [6630, 7436]]
+        self.test_name1 = "BDOR_007864-RA:cds:0"
+        self.extra_names = ["BDOR_007864-RA:cds:1", "BDOR_007864-RA:cds:2", "BDOR_007864-RA:cds:3", "BDOR_007864-RA:cds:4"]
         test_identifier1 = 8
-        extra_identifiers = [9, 10, 11, 12]
-        test_phase1 = 0
-        extra_phases = [2, 1, 0, 0]
+        self.extra_identifiers = [9, 10, 11, 12]
+        self.test_phase1 = 0
+        self.extra_phases = [2, 1, 0, 0]
         test_parent_id1 = 2
-        test_cds1 = CDS(identifier=test_identifier1, name=test_name1, indices=test_indices1, score=None, phase=test_phase1, parent_id=test_parent_id1)
-        self.assertEqual('CDS', test_cds1.__class__.__name__)
+        self.test_cds0 = CDS(identifier=test_identifier1, name=self.test_name1, indices=self.test_indices1, score=None, phase=self.test_phase1, parent_id=test_parent_id1)
+        self.test_cds1 = CDS(identifier=test_identifier1, name=self.test_name1, indices=self.test_indices1, score=None, phase=self.test_phase1, parent_id=test_parent_id1)
+        for ind_pair in self.extra_indices:
+            self.test_cds1.add_indices(ind_pair)
+        for ident in self.extra_identifiers:
+            self.test_cds1.add_identifier(ident)
+        for name in self.extra_names:
+            self.test_cds1.add_name(name)
+        for phase in self.extra_phases:
+            self.test_cds1.add_phase(phase)
+
+    def test_cds_constructor(self):
+        self.assertEquals('CDS', self.test_cds0.__class__.__name__)
         # should also be able to construct w/o all the params...
         empty_cds = CDS()
-        self.assertEqual('CDS', empty_cds.feature_type) 
+        self.assertEquals('CDS', empty_cds.feature_type) 
 
-        # test .add_indices
-        for ind_pair in extra_indices:
-            test_cds1.add_indices(ind_pair)
-        self.assertEqual([4399, 5185], test_cds1.indices[2])
+    def test_add_indices(self):
+        for ind_pair in self.extra_indices:
+            self.test_cds0.add_indices(ind_pair)
+        self.assertEquals([4399, 5185], self.test_cds0.indices[2])
 
+    def test_add_name(self):
         # test .add_name
-        for name in extra_names:
-            test_cds1.add_name(name)
-        self.assertEqual("BDOR_007864-RA:cds:4", test_cds1.name[4])
+        for name in self.extra_names:
+            self.test_cds0.add_name(name)
+        self.assertEquals("BDOR_007864-RA:cds:4", self.test_cds0.name[4])
 
-        # test .add_identifier
-        for ident in extra_identifiers:
-            test_cds1.add_identifier(ident)
+    def test_add_identifier(self):
+        for ident in self.extra_identifiers:
+            self.test_cds0.add_identifier(ident)
+        self.assertEquals(5, len(self.test_cds0.identifier))
 
-        # test .add_phase
-        for phase in extra_phases:
-            test_cds1.add_phase(phase)
+    def test_add_phase(self):
+        for phase in self.extra_phases:
+            self.test_cds0.add_phase(phase)
+        self.assertEquals(5, len(self.test_cds0.phase))
+        self.assertEquals(1, self.test_cds0.phase[2])
 
-        # test .length_of_shortest_segment
-        self.assertEqual(241, test_cds1.length_of_shortest_segment())
+    def test_length_of_shortest_segment(self):
+        self.assertEquals(241, self.test_cds1.length_of_shortest_segment())
 
-        # test .length
-        self.assertEqual(3453, test_cds1.length())
+    def test_length(self):
+        self.assertEquals(3453, self.test_cds1.length())
 
-        # test .adjust_indices
-        test_cds1.adjust_indices(-5)
-        self.assertEqual(3729, test_cds1.indices[0][0])
+    def test_adjust_indices(self):
+        self.test_cds1.adjust_indices(-5)
+        self.assertEquals(3729, self.test_cds1.indices[0][0])
         # (adjust them back so future test don't get confused :)
-        test_cds1.adjust_indices(5)
-        self.assertEqual(5185, test_cds1.indices[2][1])
+        self.test_cds1.adjust_indices(5)
+        self.assertEquals(5185, self.test_cds1.indices[2][1])
 
-        # test to_gff
+    def test_to_gff(self):
         expected1 = "sctg_0080_0020\tmaker\tCDS\t3734\t4034\t.\t+\t0\tID=8;Name=BDOR_007864-RA:cds:0;Parent=2\n"
         expected2 = "sctg_0080_0020\tmaker\tCDS\t4092\t4332\t.\t+\t2\tID=9;Name=BDOR_007864-RA:cds:1;Parent=2\n"
         expected3 = "sctg_0080_0020\tmaker\tCDS\t4399\t5185\t.\t+\t1\tID=10;Name=BDOR_007864-RA:cds:2;Parent=2\n"
         expected4 = "sctg_0080_0020\tmaker\tCDS\t5249\t6565\t.\t+\t0\tID=11;Name=BDOR_007864-RA:cds:3;Parent=2\n"
         expected5 = "sctg_0080_0020\tmaker\tCDS\t6630\t7436\t.\t+\t0\tID=12;Name=BDOR_007864-RA:cds:4;Parent=2\n"
         expected = expected1 + expected2 + expected3 + expected4 + expected5
-        actual = test_cds1.to_gff(seq_name="sctg_0080_0020", source="maker", strand='+')
-        self.assertEqual(expected, actual)
+        actual = self.test_cds1.to_gff(seq_name="sctg_0080_0020", source="maker", strand='+')
+        self.assertEquals(expected, actual)
         # what if identifier, parent_id are strings? does it matter?
-        test_cds2 = CDS(identifier='foo1', name=test_name1, indices=test_indices1, score=None, phase=test_phase1, parent_id='bar7')
+        test_cds2 = CDS(identifier='foo1', name=self.test_name1, indices=self.test_indices1, score=None, phase=self.test_phase1, parent_id='bar7')
         extra_identifiers2 = ['foo2', 'foo3', 'foo4', 'foo5']
-        for ind_pair in extra_indices:
+        for ind_pair in self.extra_indices:
             test_cds2.add_indices(ind_pair)
-        for name in extra_names:
+        for name in self.extra_names:
             test_cds2.add_name(name)
         for ident in extra_identifiers2:
             test_cds2.add_identifier(ident)
-        for phase in extra_phases:
+        for phase in self.extra_phases:
             test_cds2.add_phase(phase)
         expected1 = "sctg_0080_0020\tmaker\tCDS\t3734\t4034\t.\t+\t0\tID=foo1;Name=BDOR_007864-RA:cds:0;Parent=bar7\n"
         expected2 = "sctg_0080_0020\tmaker\tCDS\t4092\t4332\t.\t+\t2\tID=foo2;Name=BDOR_007864-RA:cds:1;Parent=bar7\n"
@@ -158,7 +179,7 @@ class TestGenePart(unittest.TestCase):
         expected5 = "sctg_0080_0020\tmaker\tCDS\t6630\t7436\t.\t+\t0\tID=foo5;Name=BDOR_007864-RA:cds:4;Parent=bar7\n"
         expected = expected1 + expected2 + expected3 + expected4 + expected5
         actual = test_cds2.to_gff(seq_name="sctg_0080_0020", source="maker", strand='+')
-        self.assertEqual(expected, actual)
+        self.assertEquals(expected, actual)
         # what if name=None?
         test_cds2.name=[]
         expected1 = "sctg_0080_0020\tmaker\tCDS\t3734\t4034\t.\t+\t0\tID=foo1;Parent=bar7\n"
@@ -168,9 +189,11 @@ class TestGenePart(unittest.TestCase):
         expected5 = "sctg_0080_0020\tmaker\tCDS\t6630\t7436\t.\t+\t0\tID=foo5;Parent=bar7\n"
         expected = expected1 + expected2 + expected3 + expected4 + expected5
         actual = test_cds2.to_gff(seq_name="sctg_0080_0020", source="maker", strand='+')
-        self.assertEqual(expected, actual)
+        self.assertEquals(expected, actual)
 
         # TODO test to_tbl when add annotations stuff
+
+class TestExon(unittest.TestCase):
 
     def test_Exon(self):
         # test constructor
@@ -184,12 +207,12 @@ class TestGenePart(unittest.TestCase):
         extra_scores = [0.9, 0.9, 0.9, 0.9]
         test_parent_id1 = 2
         test_exon1 = Exon(identifier=test_identifier1, name=test_name1, indices=test_indices1, score=test_score1, parent_id=test_parent_id1)
-        self.assertEqual('Exon', test_exon1.__class__.__name__)
+        self.assertEquals('Exon', test_exon1.__class__.__name__)
 
         # test .add_indices
         for ind_pair in extra_indices:
             test_exon1.add_indices(ind_pair)
-        self.assertEqual(5, len(test_exon1.indices))
+        self.assertEquals(5, len(test_exon1.indices))
 
         # test .add_name
         for name in extra_names:
@@ -204,14 +227,14 @@ class TestGenePart(unittest.TestCase):
             test_exon1.add_score(score)
         
         # test .length
-        self.assertEqual(3453, test_exon1.length())
+        self.assertEquals(3453, test_exon1.length())
 
         # test .adjust_indices
         test_exon1.adjust_indices(7)
-        self.assertEqual(4339, test_exon1.indices[1][1])
+        self.assertEquals(4339, test_exon1.indices[1][1])
         # adjust them back...
         test_exon1.adjust_indices(-7)
-        self.assertEqual(4332, test_exon1.indices[1][1])
+        self.assertEquals(4332, test_exon1.indices[1][1])
 
         # test .to_gff
         expected1 = "sctg_0080_0020\tmaker\texon\t3734\t4034\t0.9\t+\t.\tID=3;Name=BDOR_007864-RA:exon:0;Parent=2\n"
@@ -221,7 +244,7 @@ class TestGenePart(unittest.TestCase):
         expected5 = "sctg_0080_0020\tmaker\texon\t6630\t7436\t0.9\t+\t.\tID=7;Name=BDOR_007864-RA:exon:4;Parent=2\n"
         expected = expected1 + expected2 + expected3 + expected4 + expected5
         actual = test_exon1.to_gff(seq_name="sctg_0080_0020", source="maker", strand='+')
-        self.assertEqual(expected, actual)
+        self.assertEquals(expected, actual)
 
 
     def test_trim_end(self):
@@ -471,6 +494,8 @@ class TestGenePart(unittest.TestCase):
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(TestGenePart))
+    suite.addTest(unittest.makeSuite(TestCDS))
+    suite.addTest(unittest.makeSuite(TestExon))
     return suite
 
 if __name__ == '__main__':
