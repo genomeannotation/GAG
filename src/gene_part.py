50712660650869b8bcb668d138ed40d83a287a55
Theodore DeRego <tderego94@gmail.com>
2014-01-24 10:56:09 -1000

    Added gag.config to git ignore

--- src/gene_part.py
+++ src/gene_part.py
@@ -111,12 +111,10 @@ class GenePart:
             if start <= index[0] and stop >= index[1]:
                 index[0] = 0
                 index[1] = 0
-                continue
             # invalid region is in the middle of the index range, mark for removal
-            if start >= index[0] and stop <= index[1]:
+            elif start >= index[0] and stop <= index[1]:
                 index[0] = 0
                 index[1] = 0
-                continue
             # The beginning is in the invalid region, trim beginning forward to invalid sequence stop
             elif start < index[0] and stop >= index[0]:
                 index[0] = stop+1
