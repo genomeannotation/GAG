263d56c0cc11346681b903d462f48ea472dbd4bc
Theodore DeRego <tderego94@gmail.com>
2014-04-07 09:37:14 -1000

    Exon length range filter

--- test/filters_tests.py
+++ test/filters_tests.py
@@ -2,7 +2,8 @@
 
 import unittest
 from mock import Mock
-from src.cds_length_range_filter import CDSLengthRangeFilter
+from src.filters import CDSLengthRangeFilter
+from src.filters import ExonLengthRangeFilter
 
 class TestFilters(unittest.TestCase):
         
@@ -38,6 +39,45 @@ class TestFilters(unittest.TestCase):
         
         # Apply the filter
         cds_length_range.apply(seq)
+        
+        self.assertFalse(seq.genes[0].mrnas[0].death_flagged)
+        self.assertFalse(seq.genes[1].mrnas[0].death_flagged)
+        self.assertTrue(seq.genes[1].mrnas[1].death_flagged)
+        self.assertTrue(seq.genes[2].mrnas[0].death_flagged)
+        
+        
+    def test_exon_length_range_filter(self):
+        exon_length_range = ExonLengthRangeFilter(30, 60)
+    
+        # Create a mock sequence
+        seq = Mock()
+        
+        # Give the mock sequence some mock genes
+        seq.genes = [Mock(), Mock(), Mock()]
+        
+        # Give the mock genes some mrnas
+        seq.genes[0].mrnas = [Mock()]
+        seq.genes[1].mrnas = [Mock(), Mock()]
+        seq.genes[2].mrnas = [Mock()]
+        
+        # Give the mock mrnas some exon's
+        seq.genes[0].mrnas[0].death_flagged = False
+        seq.genes[0].mrnas[0].exon = Mock()
+        seq.genes[0].mrnas[0].exon.length = Mock(return_value=50)
+        
+        seq.genes[1].mrnas[0].death_flagged = False
+        seq.genes[1].mrnas[0].exon = None
+        
+        seq.genes[1].mrnas[1].death_flagged = False
+        seq.genes[1].mrnas[1].exon = Mock()
+        seq.genes[1].mrnas[1].exon.length = Mock(return_value=20)
+        
+        seq.genes[2].mrnas[0].death_flagged = False
+        seq.genes[2].mrnas[0].exon = Mock()
+        seq.genes[2].mrnas[0].exon.length = Mock(return_value=70)
+        
+        # Apply the filter
+        exon_length_range.apply(seq)
         
         self.assertFalse(seq.genes[0].mrnas[0].death_flagged)
         self.assertFalse(seq.genes[1].mrnas[0].death_flagged)
