f2959659b77de816a312bc2e457817cf6fbde4bd
Theodore DeRego <tderego94@gmail.com>
2014-04-07 10:52:58 -1000

    Intron length range filter

--- src/filters.py
+++ src/filters.py
@@ -32,7 +32,7 @@ class ExonLengthRangeFilter:
             for mrna in gene.mrnas:
                 if not mrna.exon:
                     gene.add_annotation("invalidated_mrna", mrna.identifier+" didn't pass exon_length_range - Exon doesn't exist")
-                    mrna.death_flagged = True # Destroy the mRNA that the cds lives on
+                    mrna.death_flagged = True # Destroy the mRNA that the exon lives on
                 elif mrna.get_shortest_exon() < self.min_length:
                     mrna.exon.add_annotation("invalidated", "didn't pass exon_length_range with Exon shorter than "+str(self.min_length))
                     mrna.death_flagged = True # Destroy the mRNA that the exon lives on
@@ -50,7 +50,10 @@ class IntronLengthRangeFilter:
     def apply(self, seq):
         for gene in seq.genes:
             for mrna in gene.mrnas:
-                if mrna.get_shortest_intron() < self.min_length:
+                if not mrna.exon:
+                    gene.add_annotation("invalidated_mrna", mrna.identifier+" didn't pass intron_length_range - Exon doesn't exist")
+                    mrna.death_flagged = True # Destroy the mRNA that the exon lives on
+                elif mrna.get_shortest_intron() < self.min_length:
                     mrna.exon.add_annotation("invalidated", "didn't pass intron_length_range with intron shorter than "+str(self.min_length))
                     mrna.death_flagged = True # Destroy the mRNA that the intron lives on
                 elif self.max_length > 0 and mrna.get_longest_intron() > self.max_length:
--- test/filters_tests.py
+++ test/filters_tests.py
@@ -2,8 +2,7 @@
 
 import unittest
 from mock import Mock
-from src.filters import CDSLengthRangeFilter
-from src.filters import ExonLengthRangeFilter
+from src.filters import *
 
 class TestFilters(unittest.TestCase):
         
@@ -89,6 +88,52 @@ class TestFilters(unittest.TestCase):
         
         # Apply the filter
         exon_length_range.apply(seq)
+        
+        self.assertFalse(seq.genes[0].mrnas[0].death_flagged)
+        self.assertFalse(seq.genes[1].mrnas[0].death_flagged)
+        self.assertTrue(seq.genes[1].mrnas[1].death_flagged)
+        self.assertTrue(seq.genes[2].mrnas[0].death_flagged)
+        
+        
+    def test_intron_length_range_filter(self):
+        intron_length_range = IntronLengthRangeFilter(30, 60)
+    
+        # Create a mock sequence
+        seq = Mock()
+        
+        # Give the mock sequence some mock genes
+        seq.genes = [Mock(), Mock(), Mock()]
+        
+        # Give the mock genes some mrnas
+        seq.genes[0].mrnas = [Mock()]
+        seq.genes[1].mrnas = [Mock(), Mock()]
+        seq.genes[2].mrnas = [Mock()]
+        
+        # Give the mock mrnas some exon's
+        seq.genes[0].mrnas[0].identifier = 'foo1-RA'
+        seq.genes[0].mrnas[0].death_flagged = False
+        seq.genes[0].mrnas[0].exon = Mock()
+        seq.genes[0].mrnas[0].get_shortest_intron = Mock(return_value=50)
+        seq.genes[0].mrnas[0].get_longest_intron = Mock(return_value=50)
+        
+        seq.genes[1].mrnas[0].identifier = 'foo2-RA'
+        seq.genes[1].mrnas[0].death_flagged = False
+        seq.genes[1].mrnas[0].exon = Mock()
+        seq.genes[1].mrnas[0].get_shortest_intron = Mock(return_value=30)
+        seq.genes[1].mrnas[0].get_longest_intron = Mock(return_value=30)
+        
+        seq.genes[1].mrnas[1].identifier = 'foo2-RB'
+        seq.genes[1].mrnas[1].death_flagged = False
+        seq.genes[1].mrnas[1].exon = None
+        
+        seq.genes[2].mrnas[0].identifier = 'foo3-RA'
+        seq.genes[2].mrnas[0].death_flagged = False
+        seq.genes[2].mrnas[0].exon = Mock()
+        seq.genes[2].mrnas[0].get_shortest_intron = Mock(return_value=70)
+        seq.genes[2].mrnas[0].get_longest_intron = Mock(return_value=70)
+        
+        # Apply the filter
+        intron_length_range.apply(seq)
         
         self.assertFalse(seq.genes[0].mrnas[0].death_flagged)
         self.assertFalse(seq.genes[1].mrnas[0].death_flagged)
