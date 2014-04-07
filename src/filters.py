659441bb47d823eb4f0b2329e2c13ccafee663b1
Theodore DeRego <tderego94@gmail.com>
2014-04-07 09:58:56 -1000

    Exon length range filter uses exon segments rather than GenePart length() method

--- src/filters.py
+++ src/filters.py
@@ -27,9 +27,26 @@ class ExonLengthRangeFilter:
     def apply(self, seq):
         for gene in seq.genes:
             for mrna in gene.mrnas:
-                if mrna.exon != None and mrna.exon.length() < self.min_length:
+                if mrna.exon != None and mrna.get_shortest_exon() < self.min_length:
                     mrna.exon.add_annotation("invalidated", "didn't pass exon_length_range with Exon shorter than "+str(self.min_length))
                     mrna.death_flagged = True # Destroy the mRNA that the exon lives on
-                elif mrna.exon != None and self.max_length > 0 and mrna.exon.length() > self.max_length:
+                elif mrna.exon != None and self.max_length > 0 and mrna.get_longest_exon() > self.max_length:
                     mrna.exon.add_annotation("invalidated", "didn't pass exon_length_range with Exon longer than "+str(self.max_length))
                     mrna.death_flagged = True # Destroy the mRNA that the exon lives on
+
+class IntronLengthRangeFilter:
+
+    def __init__(self, min_length = 0, max_length=0):
+        self.min_length = min_length
+        self.max_length = max_length
+        return
+        
+    def apply(self, seq):
+        for gene in seq.genes:
+            for mrna in gene.mrnas:
+                if mrna.cds != None and mrna.cds.length() < self.min_length:
+                    mrna.cds.add_annotation("invalidated", "didn't pass cds_length_range with CDS shorter than "+str(self.min_length))
+                    mrna.death_flagged = True # Destroy the mRNA that the cds lives on
+                elif mrna.cds != None and self.max_length > 0 and mrna.cds.length() > self.max_length:
+                    mrna.cds.add_annotation("invalidated", "didn't pass cds_length_range with CDS longer than "+str(self.max_length))
+                    mrna.death_flagged = True # Destroy the mRNA that the cds lives on
--- test/filters_tests.py
+++ test/filters_tests.py
@@ -57,31 +57,27 @@ class TestFilters(unittest.TestCase):
         
         # Give the mock genes some mrnas
         seq.genes[0].mrnas = [Mock()]
-        seq.genes[1].mrnas = [Mock(), Mock()]
+        seq.genes[1].mrnas = [Mock()]
         seq.genes[2].mrnas = [Mock()]
         
         # Give the mock mrnas some exon's
         seq.genes[0].mrnas[0].death_flagged = False
-        seq.genes[0].mrnas[0].exon = Mock()
-        seq.genes[0].mrnas[0].exon.length = Mock(return_value=50)
+        seq.genes[0].mrnas[0].get_shortest_exon = Mock(return_value=50)
+        seq.genes[0].mrnas[0].get_longest_exon = Mock(return_value=50)
         
         seq.genes[1].mrnas[0].death_flagged = False
-        seq.genes[1].mrnas[0].exon = None
-        
-        seq.genes[1].mrnas[1].death_flagged = False
-        seq.genes[1].mrnas[1].exon = Mock()
-        seq.genes[1].mrnas[1].exon.length = Mock(return_value=20)
+        seq.genes[1].mrnas[0].get_shortest_exon = Mock(return_value=30)
+        seq.genes[1].mrnas[0].get_longest_exon = Mock(return_value=30)
         
         seq.genes[2].mrnas[0].death_flagged = False
-        seq.genes[2].mrnas[0].exon = Mock()
-        seq.genes[2].mrnas[0].exon.length = Mock(return_value=70)
+        seq.genes[2].mrnas[0].get_shortest_exon = Mock(return_value=70)
+        seq.genes[2].mrnas[0].get_longest_exon = Mock(return_value=70)
         
         # Apply the filter
         exon_length_range.apply(seq)
         
         self.assertFalse(seq.genes[0].mrnas[0].death_flagged)
         self.assertFalse(seq.genes[1].mrnas[0].death_flagged)
-        self.assertTrue(seq.genes[1].mrnas[1].death_flagged)
         self.assertTrue(seq.genes[2].mrnas[0].death_flagged)
 
 
