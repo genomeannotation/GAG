ed176fd6938d32674dbc8ea7ebadba844485be6c
Woods26 <forest.bremer@gmail.com>
2017-09-13 14:51:36 -1000

    reworked seq_helper_tests.py to match fasta output code changes

--- test/seq_helper_tests.py
+++ test/seq_helper_tests.py
@@ -11,107 +11,130 @@ from src.seq_helper import SeqHelper
 class TestSeqHelper(unittest.TestCase):
     def setUp(self):
         self.helper = SeqHelper("nnnGATTACAnnnnnNNNNNgattacaNNN")
+        self.mrna = Mock()
+        self.mrna.identifier = "foo_mrna_identifier"
+        # ToDo: check if parent_id and name can be assumed to exist
+        self.mrna.parent_id = "foo_mrna_parent_id"
+        self.mrna.name = "foo_mrna_name"
+
+    def tearDown(self):
+        del self.mrna
+        del self.helper
 
     def test_initialize(self):
-        self.assertEquals("nnnGATTACAnnnnnNNNNNgattacaNNN", self.helper.full_sequence)
+        expected = "nnnGATTACAnnnnnNNNNNgattacaNNN"
+        calculated = self.helper.full_sequence
+        self.assertEquals(expected, calculated)
 
     def test_mrna_to_fasta(self):
-        mrna = Mock()
-        mrna.identifier = "foo_mrna"
-        mrna.exon = Mock()
-        mrna.exon.indices = [[4, 10], [21, 27]]
-        mrna.strand = '+'
-        expected = ">foo_mrna\nGATTACAgattaca\n"
-        self.assertEquals(expected, self.helper.mrna_to_fasta(mrna))
+        self.mrna.exon = Mock()
+        self.mrna.exon.indices = [[4, 10], [21, 27]]
+        self.mrna.strand = '+'
+        # ToDo: check with Scott if this new syntax is acceptable/desired
+        # expected = ">foo_mrna_identifier\nGATTACAgattaca\n"
+        expected = ('>foo_mrna_identifier ID=foo_mrna_identifier|Parent=foo_mrna_parent_id|Name=foo_mrna_name\n'
+                    'GATTACAgattaca\n')
+        calculated = self.helper.mrna_to_fasta(self.mrna)
+        self.assertEquals(expected, calculated)
 
     def test_mrna_to_fasta_reverse_strand(self):
-        mrna = Mock()
-        mrna.identifier = "foo_mrna"
-        mrna.exon = Mock()
-        mrna.exon.indices = [[4, 10], [21, 27]]
-        mrna.strand = '-'
-        expected = ">foo_mrna\ntgtaatcTGTAATC\n"
-        self.assertEquals(expected, self.helper.mrna_to_fasta(mrna))
+        self.mrna.exon = Mock()
+        self.mrna.exon.indices = [[4, 10], [21, 27]]
+        self.mrna.strand = '-'
+        # ToDo: check with Scott if this new syntax is acceptable/desired
+        # expected = ">foo_mrna_identifier\ntgtaatcTGTAATC\n"
+        expected = ('>foo_mrna_identifier ID=foo_mrna_identifier|Parent=foo_mrna_parent_id|Name=foo_mrna_name\n'
+                    'tgtaatcTGTAATC\n')
+        calculated = self.helper.mrna_to_fasta(self.mrna)
+        self.assertEquals(expected, calculated)
 
     def test_mrna_to_cds_fasta(self):
-        mrna = Mock()
-        mrna.identifier = "foo_mrna"
-        mrna.cds = Mock()
-        mrna.cds.indices = [[4, 10], [21, 27]]
-        mrna.strand = '-'
-        expected = ">foo_mrna CDS\ntgtaatcTGTAATC\n"
-        self.assertEquals(expected, self.helper.mrna_to_cds_fasta(mrna))
+        self.mrna.cds = Mock()
+        self.mrna.cds.indices = [[4, 10], [21, 27]]
+        self.mrna.strand = '-'
+        # ToDo: check with Scott if this new syntax is acceptable/desired
+        # expected = ">foo_mrna_identifier CDS\ntgtaatcTGTAATC\n"
+        expected = ('>CDS|foo_mrna_identifier ID=foo_mrna_identifier|Parent=foo_mrna_parent_id|Name=foo_mrna_name\n'
+                    'tgtaatcTGTAATC\n')
+        calculated = self.helper.mrna_to_cds_fasta(self.mrna)
+        self.assertEquals(expected, calculated)
 
     def test_mrna_to_protein_fasta(self):
-        mrna = Mock()
-        mrna.identifier = "foo_mrna"
-        mrna.cds = Mock()
-        mrna.cds.indices = [[4, 10], [21, 27]]
-        mrna.cds.get_phase.return_value = 0
-        mrna.strand = '+'
-        expected = ">foo_mrna protein\nDYRL\n"
-        self.assertEquals(expected, self.helper.mrna_to_protein_fasta(mrna))
+        self.mrna.cds = Mock()
+        self.mrna.cds.indices = [[4, 10], [21, 27]]
+        self.mrna.cds.get_phase.return_value = 0
+        self.mrna.strand = '+'
+        # ToDo: check with Scott if this new syntax is acceptable/desired
+        # expected = ">foo_mrna_identifier protein\nDYRL\n"
+        expected = ('>protein|foo_mrna_identifier ID=foo_mrna_identifier|Parent=foo_mrna_parent_id|Name=foo_mrna_name\n'
+                    'DYRL\n')
+        calculated = self.helper.mrna_to_protein_fasta(self.mrna)
+        self.assertEquals(expected, calculated)
 
     def test_mrna_to_protein_fasta_phase1(self):
-        mrna = Mock()
-        mrna.identifier = "foo_mrna"
-        mrna.cds = Mock()
-        mrna.cds.indices = [[4, 10], [21, 27]]
-        mrna.cds.get_phase.return_value = 1
-        mrna.strand = '+'
-        expected = ">foo_mrna protein\nITDY\n"
-        self.assertEquals(expected, self.helper.mrna_to_protein_fasta(mrna))
+        self.mrna.cds = Mock()
+        self.mrna.cds.indices = [[4, 10], [21, 27]]
+        self.mrna.cds.get_phase.return_value = 1
+        self.mrna.strand = '+'
+        # ToDo: check with Scott if this new syntax is acceptable/desired
+        # expected = ">foo_mrna_identifier protein\nITDY\n"
+        expected = ('>protein|foo_mrna_identifier ID=foo_mrna_identifier|Parent=foo_mrna_parent_id|Name=foo_mrna_name\n'
+                    'ITDY\n')
+        calculated = self.helper.mrna_to_protein_fasta(self.mrna)
+        self.assertEquals(expected, calculated)
 
     def test_mrna_to_protein_fasta_reverse(self):
-        mrna = Mock()
-        mrna.identifier = "foo_mrna"
-        mrna.cds = Mock()
-        mrna.cds.indices = [[21, 27], [4, 10]]
-        mrna.cds.get_phase.return_value = 0
-        mrna.strand = '-'
-        expected = ">foo_mrna protein\nCNL*\n"
-        self.assertEquals(expected, self.helper.mrna_to_protein_fasta(mrna))
+        self.mrna.cds = Mock()
+        self.mrna.cds.indices = [[21, 27], [4, 10]]
+        self.mrna.cds.get_phase.return_value = 0
+        self.mrna.strand = '-'
+        # ToDo: check with Scott if this new syntax is acceptable/desired
+        # expected = ">foo_mrna_identifier protein\nCNL*\n"
+        expected = ('>protein|foo_mrna_identifier ID=foo_mrna_identifier|Parent=foo_mrna_parent_id|Name=foo_mrna_name\n'
+                    'CNL*\n')
+        calculated = self.helper.mrna_to_protein_fasta(self.mrna)
+        self.assertEquals(expected, calculated)
 
     def test_mrna_to_protein_fasta_reverse_phase1(self):
-        mrna = Mock()
-        mrna.identifier = "foo_mrna"
-        mrna.cds = Mock()
-        mrna.cds.indices = [[21, 27], [4, 10]]
-        mrna.cds.get_phase.return_value = 1
-        mrna.strand = '-'
-        expected = ">foo_mrna protein\nVICN\n"
-        self.assertEquals(expected, self.helper.mrna_to_protein_fasta(mrna))
+        self.mrna.cds = Mock()
+        self.mrna.cds.indices = [[21, 27], [4, 10]]
+        self.mrna.cds.get_phase.return_value = 1
+        self.mrna.strand = '-'
+        # ToDo: check with Scott if this new syntax is acceptable/desired
+        # expected = ">foo_mrna_identifier protein\nVICN\n"
+        expected = ('>protein|foo_mrna_identifier ID=foo_mrna_identifier|Parent=foo_mrna_parent_id|Name=foo_mrna_name\n'
+                    'VICN\n')
+        calculated = self.helper.mrna_to_protein_fasta(self.mrna)
+        self.assertEquals(expected, calculated)
 
     def test_get_sequence_from_indices(self):
-        mrna = Mock()
-        mrna.identifier = "foo_mrna"
-        mrna.cds = Mock()
-        mrna.cds.indices = [[21, 27], [4, 10]]
-        mrna.strand = '+'
+        self.mrna.cds = Mock()
+        self.mrna.cds.indices = [[21, 27], [4, 10]]
+        self.mrna.strand = '+'
         expected = 'gattacaGATTACA'
-        calculated = self.helper.get_sequence_from_indices(mrna.strand, mrna.cds.indices)
+        calculated = self.helper.get_sequence_from_indices(self.mrna.strand, self.mrna.cds.indices)
         self.assertEquals(expected, calculated)
 
     def test_get_sequence_from_indices_reverse(self):
-        mrna = Mock()
-        mrna.identifier = "foo_mrna"
-        mrna.cds = Mock()
-        mrna.cds.indices = [[21, 27], [4, 10]]
-        mrna.strand = '-'
+        self.mrna.cds = Mock()
+        self.mrna.cds.indices = [[21, 27], [4, 10]]
+        self.mrna.strand = '-'
         expected = 'TGTAATCtgtaatc'
-        calculated = self.helper.get_sequence_from_indices(mrna.strand, mrna.cds.indices)
+        calculated = self.helper.get_sequence_from_indices(self.mrna.strand, self.mrna.cds.indices)
         self.assertEquals(expected, calculated)
 
     def test_mrna_contains_internal_stop(self):
         helper = SeqHelper("gattacaTAGgattaca")  # TAG = stop codon
-        mrna = Mock()
-        mrna.strand = '+'
-        mrna.cds = Mock()
-        mrna.cds.indices = [[2, 4], [8, 14]]
+        self.mrna.strand = '+'
+        self.mrna.cds = Mock()
+        self.mrna.cds.indices = [[2, 4], [8, 14]]
         # verify that the internal stop is in the sequence ...
-        self.assertEquals("attTAGgatt", helper.get_sequence_from_indices('+', mrna.cds.indices))
+        expected = "attTAGgatt"
+        calculated = helper.get_sequence_from_indices('+', self.mrna.cds.indices)
+        self.assertEquals(expected, calculated)
         # make sure the method catches it
-        self.assertTrue(helper.mrna_contains_internal_stop(mrna))
+        test = helper.mrna_contains_internal_stop(self.mrna)
+        self.assertTrue(test)
 
 
 def suite():
